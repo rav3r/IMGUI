9d602fb12aa76ba6851d7dae86e72cf887a454d2
unknown <rafal.gawel@gmail.com>
2013-03-27 14:10:18 +0100

    Simple SFML window test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	sf::RectangleShape shape(sf::Vector2f(400, 300));
+	shape.setPosition(sf::Vector2f(200, 150));
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
