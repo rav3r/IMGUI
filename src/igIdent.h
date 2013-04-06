a2446a86e118e556fb1de1133f34d7931017bbd2
Rafal Gawel <rafal.gawel@gmail.com>
2013-04-06 12:22:07 +0200

    Adding renderer (interface and simple implementation)

--- src/igIdent.h
+++ src/igIdent.h
@@ -8,27 +8,30 @@
 #endif
 
 #define GEN_ID(var) igIdent((void*)&var, IMGUI_LINE_ID)
+#define GEN_IID(i) igIdent(i, IMGUI_LINE_ID)
 #define GEN_NULL_ID igIdent(0, IMGUI_LINE_ID)
 
 struct igIdent
 {
 	void* variable;
 	int line;
+	int num;
 
-	igIdent(void* v, int l): variable(v), line(l) {}	
-	igIdent(const igIdent& id): variable(id.variable), line(id.line) {}
-	igIdent(): variable(0), line(0) {}
+	igIdent(void* v, int l): variable(v), line(l), num(0) {}
+	igIdent(int n, int l): variable(0), line(l), num(n) {}
+	igIdent(const igIdent& id): variable(id.variable), line(id.line), num(id.num) {}
+	igIdent(): variable(0), line(0), num(0) {}
 	
 };
 
 inline bool operator==(const igIdent& a, const igIdent& b)
 {
-	return a.variable == b.variable && a.line == b.line;
+	return a.variable == b.variable && a.line == b.line && a.num == b.num;
 }
 
 inline bool operator!=(const igIdent& a, const igIdent& b)
 {
-	return a.variable != b.variable || a.line != b.line;
+	return a.variable != b.variable || a.line != b.line || a.num != b.num;
 }
 
 #endif
